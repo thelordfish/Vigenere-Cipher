71dbea6eaef1ac35ed82877b55138bfb61468f28
thelordfish <168198925+thelordfish@users.noreply.github.com>
2024-10-08 19:33:44 +0100

    Update vigenere cipher.py

--- vigenere cipher.py
+++ vigenere cipher.py
@@ -1,1 +1,25 @@
-plaintext
+plaintext = "Hello World".lower()
+key = "Ollie".lower()
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+numkey = []
+encryption =""
+for n in key:
+    numkey.append(alphabet.find(n))
+
+print(numkey)
+input()
+counter = 0
+for l in plaintext:
+    if l == " ":
+        encryption = encryption + " "
+        continue
+    location = alphabet.find(l)
+    newlocation = location + numkey[counter]
+    if newlocation >= 26:
+        newlocation = newlocation - 26
+    encryption = encryption + alphabet[newlocation]
+    counter = counter + 1
+    if counter >= len(numkey):
+        counter = 0
+
+print(encryption)
