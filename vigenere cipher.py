88cdf7979f0c365ca6a50304c54f9539bc83dc68
thelordfish <168198925+thelordfish@users.noreply.github.com>
2024-10-08 19:49:42 +0100

    Update vigenere cipher.py
    added helpful comments

--- vigenere cipher.py
+++ vigenere cipher.py
@@ -1,22 +1,29 @@
-plaintext = "Hello World".lower()
-key = "Ollie".lower()
+plaintext = "we are discovered save yourself".lower()
+key = "deceptive".lower()
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 numkey = []
 encryption =""
+
+#turn the key into it's numbers in the alphabet
 for n in key:
     numkey.append(alphabet.find(n))
 
-# print(numkey)
-# input()
+print(numkey)
+input()
 counter = 0
 for l in plaintext:
+#if it's a space, dont increment the counter and continue
     if l == " ":
-        encryption = encryption + " "
         continue
+#find the cipher letter by adding the key number to the location of the plaintext
     location = alphabet.find(l)
     newlocation = location + numkey[counter]
+
+#loop back to the start of the alphabet if it's going over
     if newlocation >= 26:
         newlocation = newlocation - 26
+
+#add cipher letter to result
     encryption = encryption + alphabet[newlocation]
     counter = counter + 1
     if counter >= len(numkey):
